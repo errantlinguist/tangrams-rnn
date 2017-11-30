fd483879bf28f6571236573f9e9d4756353c47c0
Todd Shore <tcshore@kth.se>
2017-11-30 11:17:11 +0100

    added description and license header

--- scripts/batch-cross-validation.bash
+++ scripts/batch-cross-validation.bash
@@ -1,6 +1,29 @@
 #!/bin/bash -l
 # The -l above is required to get the full environment with modules
 
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+# 	http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+
+# A batch script for running cross-validation using Slurm <https://slurm.schedmd.com/>.
+# 
+# Author: Todd Shore <tcshore@kth.se>
+# Since: 2017-11-28
+
 # Set the allocation to be charged for this job
 # not required if you have set a default allocation
 #SBATCH -A 2017-130
