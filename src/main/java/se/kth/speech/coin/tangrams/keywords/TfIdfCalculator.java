57f82bdc2e6b8f626d09dbd565c5d455a6d5c565
Todd Shore <tcshore@kth.se>
2017-12-01 18:04:37 +0100

    started TfIdfKeywordWriter

---
+++ src/main/java/se/kth/speech/coin/tangrams/keywords/TfIdfCalculator.java
@@ -0,0 +1,106 @@
+/*
+ * 	Copyright 2017 Todd Shore
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ */
+package se.kth.speech.coin.tangrams.keywords;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.IdentityHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.function.ToDoubleBiFunction;
+
+import se.kth.speech.HashedCollections;
+import se.kth.speech.coin.tangrams.wac.data.Session;
+
+/**
+ * @author <a href="mailto:tcshore@kth.se">Todd Shore</a>
+ * @since Dec 1, 2017
+ *
+ */
+public final class TfIdfCalculator<T> implements ToDoubleBiFunction<T, Session> {
+
+	private static final int DEFAULT_INITIAL_WORD_MAP_CAPACITY = HashedCollections.capacity(1000);
+
+	public static <T> TfIdfCalculator<T> create(final Map<Session, List<T>> sessionObservations,
+			final boolean onlyInstructor) {
+		final int initialMapCapcity = HashedCollections.capacity(sessionObservations.size());
+		final Map<Session, Map<T, Double>> observationCountsPerSession = new IdentityHashMap<>(initialMapCapcity);
+		final Map<T, Set<Session>> observationSessions = new HashMap<>(DEFAULT_INITIAL_WORD_MAP_CAPACITY);
+		for (final Entry<Session, List<T>> entry : sessionObservations.entrySet()) {
+			final Session session = entry.getKey();
+			final Map<T, Double> sessionTokenCounts = observationCountsPerSession.computeIfAbsent(session,
+					key -> new HashMap<>(DEFAULT_INITIAL_WORD_MAP_CAPACITY));
+			final List<T> observations = entry.getValue();
+			observations.forEach(observation -> {
+				sessionTokenCounts.compute(observation, (key, oldValue) -> oldValue == null ? 1 : oldValue + 1);
+				observationSessions.computeIfAbsent(observation, key -> new HashSet<>(initialMapCapcity)).add(session);
+			});
+		}
+
+		return new TfIdfCalculator<>(observationCountsPerSession, observationSessions, sessionObservations.size());
+	}
+
+	private final Map<Session, Map<T, Double>> observationCountsPerSession;
+
+	private final Map<T, Set<Session>> observationSessions;
+
+	private final double totalSessionCount;
+
+	private TfIdfCalculator(final Map<Session, Map<T, Double>> observationCountsPerSession,
+			final Map<T, Set<Session>> observationSessions, final int totalSessionCount) {
+		this.observationCountsPerSession = observationCountsPerSession;
+		this.observationSessions = observationSessions;
+		this.totalSessionCount = totalSessionCount;
+	}
+
+	@Override
+	public double applyAsDouble(final T observation, final Session session) {
+		final double tf = tf(observation, session);
+		final double idf = idf(observation);
+		final double result = tf / idf;
+		assert Double.isFinite(result) : String.format("TF-IDF score for \"%s\" is not finite for session \"%s\".",
+				observation, session.getName());
+		return result;
+	}
+
+	private double df(final T observation) {
+		final Set<Session> sessions = observationSessions.get(observation);
+		assert sessions != null : String.format("Session set for \"%s\" is null.", observation);
+		final int result = sessions.size();
+		assert result > 0 : String.format("Session set for \"%s\" is of size %d.", observation, result);
+		return result;
+	}
+
+	private double idf(final T observation) {
+		final double df = df(observation);
+		final double result = Math.log(totalSessionCount / df);
+		assert Double.isFinite(result) : String.format("IDF score for \"%s\" is not finite.", observation);
+		return result;
+	}
+
+	private double tf(final T observation, final Session session) {
+		final Map<T, Double> sessionWordCounts = observationCountsPerSession.get(session);
+		assert sessionWordCounts != null;
+		final Double result = sessionWordCounts.get(observation);
+		assert result != null : String.format("Term frequency for \"%s\" is null for session \"%s\".", observation,
+				session.getName());
+		assert Double.isFinite(result) : String.format("Term frequency for \"%s\" is not finite for session \"%s\".",
+				observation, session.getName());
+		return result;
+	}
+}
---
+++ src/main/java/se/kth/speech/coin/tangrams/keywords/TfIdfKeywordWriter.java
@@ -0,0 +1,184 @@
+/*
+ * 	Copyright 2017 Todd Shore
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ */
+package se.kth.speech.coin.tangrams.keywords;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+import java.util.function.Supplier;
+import java.util.function.ToDoubleFunction;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import se.kth.speech.ListSubsequences;
+import se.kth.speech.coin.tangrams.wac.data.Round;
+import se.kth.speech.coin.tangrams.wac.data.Session;
+import se.kth.speech.coin.tangrams.wac.data.SessionSetReader;
+import se.kth.speech.coin.tangrams.wac.data.Utterance;
+import se.kth.speech.coin.tangrams.wac.logistic.Weighted;
+
+/**
+ * @author <a href="mailto:tcshore@kth.se">Todd Shore</a>
+ * @since Dec 1, 2017
+ *
+ */
+public final class TfIdfKeywordWriter {
+
+	private enum Parameter implements Supplier<Option> {
+		HELP("?") {
+			@Override
+			public Option get() {
+				return Option.builder(optName).longOpt("help").desc("Prints this message.").build();
+			}
+		},
+		REFERRING_TOKENS("t") {
+			@Override
+			public Option get() {
+				return Option.builder(optName).longOpt("referring-tokens")
+						.desc("The file to read utterance referring-language mappings from.").hasArg().argName("path")
+						.type(File.class).required().build();
+			}
+		};
+
+		private static final Options OPTIONS = createOptions();
+
+		private static Options createOptions() {
+			final Options result = new Options();
+			Arrays.stream(Parameter.values()).map(Parameter::get).forEach(result::addOption);
+			return result;
+		}
+
+		private static void printHelp() {
+			final HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp(TfIdfKeywordWriter.class.getName() + " INPATHS...", OPTIONS);
+		}
+
+		protected final String optName;
+
+		private Parameter(final String optName) {
+			this.optName = optName;
+		}
+
+	}
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(TfIdfKeywordWriter.class);
+
+	private static final Collector<CharSequence, ?, String> TOKEN_JOINER = Collectors.joining(" ");
+
+	public static void main(final CommandLine cl) throws ParseException, IOException { // NO_UCD
+																						// (use
+																						// private)
+		if (cl.hasOption(Parameter.HELP.optName)) {
+			Parameter.printHelp();
+		} else {
+			final Path[] inpaths = cl.getArgList().stream().map(Paths::get).toArray(Path[]::new);
+			if (inpaths.length < 1) {
+				throw new ParseException("No input paths specified.");
+			} else {
+				LOGGER.info("Will read sessions from {}.", Arrays.toString(inpaths));
+				final Path refTokenFilePath = ((File) cl.getParsedOptionValue(Parameter.REFERRING_TOKENS.optName))
+						.toPath();
+				final NavigableMap<Session, List<List<String>>> sessionNgrams = new TreeMap<>(
+						Comparator.comparing(Session::getName));
+				new SessionSetReader(refTokenFilePath).apply(inpaths).getSessions().forEach(
+						session -> sessionNgrams.put(session, createNgrams(session).collect(Collectors.toList())));
+				LOGGER.info("Will extract keywords from {} session(s).", sessionNgrams.size());
+				final TfIdfCalculator<List<String>> tfidfCalculator = TfIdfCalculator.create(sessionNgrams, false);
+
+				try (CSVPrinter printer = CSVFormat.TDF.withHeader("SESSION", "NGRAM", "TF-IDF").print(System.out)) {
+					for (final Entry<Session, List<List<String>>> entry : sessionNgrams.entrySet()) {
+						final Session session = entry.getKey();
+						final List<List<String>> ngrams = entry.getValue();
+						final ToDoubleFunction<List<String>> ngramWeighter = word -> tfidfCalculator.applyAsDouble(word,
+								session);
+						final Stream<Weighted<List<String>>> scoredNgrams = ngrams.stream()
+								.map(ngram -> new Weighted<>(ngram, ngramWeighter.applyAsDouble(ngram)))
+								.sorted(Comparator.reverseOrder());
+
+						final String sessionName = session.getName();
+						final Stream<Stream<Object>> cellValues = scoredNgrams.map(scoredNgram -> Stream.of(sessionName,
+								scoredNgram.getWrapped().stream().collect(TOKEN_JOINER), scoredNgram.getWeight()));
+						final Stream<List<String>> rows = cellValues
+								.map(stream -> stream.map(Object::toString).collect(Collectors.toList()));
+						printer.printRecords((Iterable<List<String>>) rows::iterator);
+					}
+				}
+			}
+		}
+	}
+
+	public static void main(final String[] args) throws IOException {
+		final CommandLineParser parser = new DefaultParser();
+		try {
+			final CommandLine cl = parser.parse(Parameter.OPTIONS, args);
+			main(cl);
+		} catch (final ParseException e) {
+			System.out.println(String.format("An error occurred while parsing the command-line arguments: %s", e));
+			Parameter.printHelp();
+		}
+	}
+
+	private static Stream<List<String>> createNgrams(final List<String> tokenSeq) {
+		final Stream<List<String>> result;
+		switch (tokenSeq.size()) {
+		case 0: {
+			result = Stream.empty();
+			break;
+		}
+		case 1: {
+			result = Stream.of(tokenSeq);
+			break;
+		}
+		default: {
+			result = IntStream.rangeClosed(1, tokenSeq.size())
+					.mapToObj(n -> ListSubsequences.createSubsequenceList(tokenSeq, n)).flatMap(List::stream);
+			break;
+		}
+		}
+		return result;
+	}
+
+	private static Stream<List<String>> createNgrams(final Session session) {
+		final Stream<List<String>> uttTokenSeqs = session.getRounds().stream().map(Round::getUtts).flatMap(List::stream)
+				.map(Utterance::getTokens);
+		return uttTokenSeqs.flatMap(TfIdfKeywordWriter::createNgrams);
+	}
+
+	private TfIdfKeywordWriter() {
+	}
+
+}
