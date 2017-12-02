c8335ce7fc1fcdeb0f85e16a3ba81e049557a0ff
Todd Shore <tcshore@kth.se>
2017-12-02 13:26:43 +0100

    now creating n-grams using "weka.core.tokenizers.NGramTokenizer"

--- src/main/java/se/kth/speech/coin/tangrams/keywords/TfIdfKeywordWriter.java
+++ src/main/java/se/kth/speech/coin/tangrams/keywords/TfIdfKeywordWriter.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map.Entry;
@@ -29,7 +30,6 @@ import java.util.function.Supplier;
 import java.util.function.ToDoubleFunction;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import org.apache.commons.cli.CommandLine;
@@ -44,12 +44,12 @@ import org.apache.commons.csv.CSVPrinter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import se.kth.speech.ListSubsequences;
 import se.kth.speech.coin.tangrams.wac.data.Round;
 import se.kth.speech.coin.tangrams.wac.data.Session;
 import se.kth.speech.coin.tangrams.wac.data.SessionSetReader;
 import se.kth.speech.coin.tangrams.wac.data.Utterance;
 import se.kth.speech.coin.tangrams.wac.logistic.Weighted;
+import weka.core.tokenizers.NGramTokenizer;
 
 /**
  * @author <a href="mailto:tcshore@kth.se">Todd Shore</a>
@@ -97,7 +97,14 @@ public final class TfIdfKeywordWriter {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(TfIdfKeywordWriter.class);
 
-	private static final Collector<CharSequence, ?, String> TOKEN_JOINER = Collectors.joining(" ");
+	private static final String TOKEN_DELIMITER;
+
+	private static final Collector<CharSequence, ?, String> TOKEN_JOINER;
+
+	static {
+		TOKEN_DELIMITER = " ";
+		TOKEN_JOINER = Collectors.joining(TOKEN_DELIMITER);
+	}
 
 	public static void main(final CommandLine cl) throws ParseException, IOException { // NO_UCD
 																						// (use
@@ -114,9 +121,15 @@ public final class TfIdfKeywordWriter {
 						.toPath();
 				final NavigableMap<Session, List<List<String>>> sessionNgrams = new TreeMap<>(
 						Comparator.comparing(Session::getName));
+
 				new SessionSetReader(refTokenFilePath).apply(inpaths).getSessions().forEach(
 						session -> sessionNgrams.put(session, createNgrams(session).collect(Collectors.toList())));
 				LOGGER.info("Will extract keywords from {} session(s).", sessionNgrams.size());
+				assert containsNgram(sessionNgrams.values(), Arrays.asList("okay")) : String
+						.format("Set of all session n-grams does not contain %s.", Arrays.asList("okay"));
+				assert containsNgram(sessionNgrams.values(), Arrays.asList("a")) : String
+						.format("Set of all session n-grams does not contain %s.", Arrays.asList("a"));
+
 				final TfIdfCalculator<List<String>> tfidfCalculator = TfIdfCalculator.create(sessionNgrams, false);
 
 				try (CSVPrinter printer = CSVFormat.TDF.withHeader("SESSION", "NGRAM", "TF-IDF").print(System.out)) {
@@ -152,24 +165,26 @@ public final class TfIdfKeywordWriter {
 		}
 	}
 
+	private static boolean containsNgram(final Collection<? extends Collection<List<String>>> sessionNgrams,
+			final List<String> ngram) {
+		final Stream<List<String>> allNgrams = sessionNgrams.stream().flatMap(Collection::stream);
+		return allNgrams.anyMatch(ngram::equals);
+	}
+
 	private static Stream<List<String>> createNgrams(final List<String> tokenSeq) {
-		final Stream<List<String>> result;
-		switch (tokenSeq.size()) {
-		case 0: {
-			result = Stream.empty();
-			break;
+		final NGramTokenizer tokenizer = new NGramTokenizer();
+		tokenizer.setDelimiters(TOKEN_DELIMITER);
+		tokenizer.setNGramMinSize(1);
+		tokenizer.setNGramMaxSize(tokenSeq.size());
+		final String inputStr = tokenSeq.stream().collect(TOKEN_JOINER);
+		tokenizer.tokenize(inputStr);
+		final Stream.Builder<List<String>> resultBuilder = Stream.builder();
+		while (tokenizer.hasMoreElements()) {
+			final String nextStr = tokenizer.nextElement();
+			final List<String> ngram = Arrays.asList(nextStr.split(TOKEN_DELIMITER));
+			resultBuilder.accept(ngram);
 		}
-		case 1: {
-			result = Stream.of(tokenSeq);
-			break;
-		}
-		default: {
-			result = IntStream.rangeClosed(1, tokenSeq.size())
-					.mapToObj(n -> ListSubsequences.createSubsequenceList(tokenSeq, n)).flatMap(List::stream);
-			break;
-		}
-		}
-		return result;
+		return resultBuilder.build();
 	}
 
 	private static Stream<List<String>> createNgrams(final Session session) {
