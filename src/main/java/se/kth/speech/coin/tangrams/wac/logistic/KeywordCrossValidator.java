5dcdc2b69626dd3964598a48e6928f35e2dffcab
Todd Shore <tcshore@kth.se>
2017-12-02 18:25:56 +0100

    created skeleton for class "KeywordCrossValidator"

---
+++ src/main/java/se/kth/speech/coin/tangrams/wac/logistic/KeywordCrossValidator.java
@@ -0,0 +1,149 @@
+/*
+ * 	Copyright 2017 Todd Shore
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ */
+package se.kth.speech.coin.tangrams.wac.logistic;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.concurrent.ForkJoinPool;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import se.kth.speech.coin.tangrams.wac.data.Session;
+import se.kth.speech.coin.tangrams.wac.data.SessionSet;
+import se.kth.speech.coin.tangrams.wac.data.SessionSetReader;
+
+public final class KeywordCrossValidator { // NO_UCD (use default)
+
+	public static final class Exception extends RuntimeException { // NO_UCD
+																	// (use
+																	// private)
+
+		/**
+		 *
+		 */
+		private static final long serialVersionUID = -1636897113752283942L;
+
+		private static final String createMessage(final SessionSet training, final Session testing,
+				final java.lang.Exception cause) {
+			final Set<String> trainingSessionNames = training.getSessions().stream().map(Session::getName)
+					.collect(Collectors.toCollection(() -> new TreeSet<>()));
+			return String.format(
+					"A(n) %s occurred while cross-validating with a training set of %d session(s) and testing on session \"%s\". Training sets: %s",
+					cause, training.size(), testing.getName(), trainingSessionNames);
+		}
+
+		private Exception(final SessionSet training, final Session testing, final java.lang.Exception cause) {
+			super(createMessage(training, testing, cause), cause);
+		}
+
+	}
+
+	private enum Parameter implements Supplier<Option> {
+		HELP("?") {
+			@Override
+			public Option get() {
+				return Option.builder(optName).longOpt("help").desc("Prints this message.").build();
+			}
+		},
+		REFERRING_TOKENS("t") {
+			@Override
+			public Option get() {
+				return Option.builder(optName).longOpt("referring-tokens")
+						.desc("The file to read utterance referring-language mappings from.").hasArg().argName("path")
+						.type(File.class).required().build();
+			}
+		};
+
+		protected final String optName;
+
+		private Parameter(final String optName) {
+			this.optName = optName;
+		}
+
+	}
+
+	private static final Options OPTIONS = createOptions();
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(KeywordCrossValidator.class);
+
+	public static void main(final CommandLine cl) throws ParseException, IOException { // NO_UCD
+																						// (use
+																						// private)
+		if (cl.hasOption(Parameter.HELP.optName)) {
+			printHelp();
+		} else {
+			final Path[] inpaths = cl.getArgList().stream().map(Paths::get).toArray(Path[]::new);
+			if (inpaths.length < 1) {
+				throw new ParseException("No input paths specified.");
+			} else {
+				LOGGER.info("Will read sessions from {}.", Arrays.toString(inpaths));
+				final Map<ModelParameter, Object> modelParams = ModelParameter.createParamValueMap(cl);
+				final Path refTokenFilePath = ((File) cl.getParsedOptionValue(Parameter.REFERRING_TOKENS.optName))
+						.toPath();
+				final SessionSet set = new SessionSetReader(refTokenFilePath).apply(inpaths);
+				LOGGER.info("Will run cross-validation using {} session(s).", set.size());
+				final ForkJoinPool executor = ForkJoinPool.commonPool();
+				// NOTE: No need to explicitly shut down common pool
+				LOGGER.info("Will run cross-validation using a(n) {} instance with a parallelism level of {}.",
+						executor.getClass().getSimpleName(), executor.getParallelism());
+				final Supplier<LogisticModel> modelFactory = () -> new LogisticModel(modelParams, executor);
+				final CrossValidator crossValidator = new CrossValidator(modelParams, modelFactory, executor);
+				// TODO: Finish
+			}
+		}
+	}
+
+	public static void main(final String[] args) throws IOException {
+		final CommandLineParser parser = new DefaultParser();
+		try {
+			final CommandLine cl = parser.parse(OPTIONS, args);
+			main(cl);
+		} catch (final ParseException e) {
+			System.out.println(String.format("An error occurred while parsing the command-line arguments: %s", e));
+			printHelp();
+		}
+	}
+
+	private static Options createOptions() {
+		final Options result = new Options();
+		Arrays.stream(Parameter.values()).map(Parameter::get).forEach(result::addOption);
+		Arrays.stream(ModelParameter.values()).map(ModelParameter::createCLIOptionBuilder).map(Option.Builder::build)
+				.forEach(result::addOption);
+		return result;
+	}
+
+	private static void printHelp() {
+		final HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp(KeywordCrossValidator.class.getName() + " INPATHS...", OPTIONS);
+	}
+
+}
