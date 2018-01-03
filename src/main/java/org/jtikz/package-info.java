b6e43ff888150d0c9d19dea999fc546b14428844
Todd Shore <tcshore@kth.se>
2018-01-03 15:34:04 +0100

    embedded JTikZ into project <http://jtikz.sourceforge.net/>

---
+++ src/main/java/org/jtikz/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * A re-packaging of JTikZ.
+ *
+ * @since 2018-01-03
+ * @version 2013-04-19
+ * @author <a href="https://www.sultanik.com/">Evan A. Sultanik</a>
+ * @see <a href="http://jtikz.sourceforge.net/">TikZ website</a>
+ */
+package org.jtikz;
