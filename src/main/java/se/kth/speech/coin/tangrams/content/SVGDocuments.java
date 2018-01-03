739563b392d494018a604b3f16c7606131f8b4d1
errantlinguist <errantlinguist+github@gmail.com>
2018-01-03 23:36:33 +0100

    now returning SVGDocument instead of just Document

---
+++ src/main/java/se/kth/speech/coin/tangrams/content/SVGDocuments.java
@@ -0,0 +1,66 @@
+/*
+ * 	Copyright 2017 Todd Shore
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ */
+package se.kth.speech.coin.tangrams.content;
+
+import java.io.IOException;
+
+import org.apache.batik.anim.dom.SAXSVGDocumentFactory;
+import org.apache.batik.util.XMLResourceDescriptor;
+import org.w3c.dom.svg.SVGDocument;
+
+/**
+ * This class is thread-safe.
+ *
+ * @author <a href="mailto:errantlinguist+github@gmail.com>Todd Shore</a>
+ * @since 3 Jan 2018
+ *
+ */
+final class SVGDocuments {
+
+	private static final ThreadLocal<SAXSVGDocumentFactory> SVG_DOC_FACTORY = new ThreadLocal<SAXSVGDocumentFactory>() {
+
+		private final String xmlParserName = XMLResourceDescriptor.getXMLParserClassName();
+
+		/*
+		 * (non-Javadoc)
+		 *
+		 * @see java.lang.ThreadLocal#initialValue()
+		 */
+		@Override
+		protected SAXSVGDocumentFactory initialValue() {
+			return new SAXSVGDocumentFactory(xmlParserName);
+		}
+
+	};
+
+	/**
+	 * Uses a {@link SAXSVGDocumentFactory} to parse the given URI into a DOM.
+	 *
+	 * @param uri
+	 *            A URI identifying the SVG file to read.
+	 * @return A {@link SVGDocument} instance that represents the SVG file.
+	 * @throws IOException
+	 *             if an error occurred while reading the document.
+	 */
+	static SVGDocument read(final String uri) throws IOException {
+		final SAXSVGDocumentFactory factory = SVG_DOC_FACTORY.get();
+		return factory.createSVGDocument(uri);
+	}
+
+	private SVGDocuments() {
+	}
+
+}
