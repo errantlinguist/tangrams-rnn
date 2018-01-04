0db38f687de3476e61a0b782cb348fe6e2e1fdaf
Todd Shore <tcshore@kth.se>
2018-01-04 10:23:25 +0100

    renamed class

--- src/main/java/se/kth/speech/coin/tangrams/content/SVGToPNGWriter.java
+++ src/main/java/se/kth/speech/coin/tangrams/content/SVGToPNGWriter.java
@@ -44,19 +44,33 @@ import org.w3c.dom.NodeList;
  * @since 7 Mar 2017
  *
  */
-public final class SVGFileToPNGConverter {
+public final class SVGToPNGWriter {
 
 	private static final Pattern LENGTH_MEASUREMENT_PATTERN = Pattern.compile("(\\d+(?:\\.\\d+)?)(\\S*)");
-	
+
+	public static void main(final String[] args) throws TranscoderException, IOException, URISyntaxException {
+		if (args.length != 2) {
+			System.err.println(String.format("Usage: %s <infile> <outfile>", SVGToPNGWriter.class.getName()));
+			System.exit(64);
+		} else {
+			final Path infilePath = Paths.get(args[0]);
+			final Path outfilePath = Paths.get(args[1]);
+			System.out.print(infilePath + " > ");
+			writeSVGAsPNG(infilePath.toUri(), outfilePath);
+			System.out.println(outfilePath);
+		}
+
+	}
+
 	/**
-	 * @see <a href= "http://stackoverflow.com/q/32721467/1391325">StackOverflow</a>
+	 * @see <a href=
+	 *      "http://stackoverflow.com/q/32721467/1391325">StackOverflow</a>
 	 * @param doc
 	 * @param outpath
 	 * @throws TranscoderException
 	 * @throws IOException
 	 */
-	public static void convertSVGToPNG(final Document doc, final Path outpath)
-			throws TranscoderException, IOException {
+	public static void writeSVGAsPNG(final Document doc, final Path outpath) throws TranscoderException, IOException {
 		final ByteArrayOutputStream resultByteStream = new ByteArrayOutputStream();
 		final TranscoderInput transcoderInput = new TranscoderInput(doc);
 		final TranscoderOutput transcoderOutput = new TranscoderOutput(resultByteStream);
@@ -78,30 +92,15 @@ public final class SVGFileToPNGConverter {
 	}
 
 	/**
-	 * @see <a href= "http://stackoverflow.com/q/32721467/1391325">StackOverflow</a>
+	 * @see <a href=
+	 *      "http://stackoverflow.com/q/32721467/1391325">StackOverflow</a>
 	 * @param inputUri
 	 * @param outpath
 	 * @throws TranscoderException
 	 * @throws IOException
 	 */
-	public static void convertSVGToPNG(final URI inputUri, final Path outpath)
-			throws TranscoderException, IOException {
-		convertSVGToPNG(SVGDocuments.read(inputUri), outpath);
-	}
-
-	public static void main(final String[] args) throws TranscoderException, IOException, URISyntaxException {
-		if (args.length != 2) {
-			System.err
-					.println(String.format("Usage: %s <infile> <outfile>", SVGFileToPNGConverter.class.getName()));
-			System.exit(64);
-		} else {
-			final Path infilePath = Paths.get(args[0]);
-			final Path outfilePath = Paths.get(args[1]);
-			System.out.print(infilePath + " > ");
-			convertSVGToPNG(infilePath.toUri(), outfilePath);
-			System.out.println(outfilePath);
-		}
-
+	public static void writeSVGAsPNG(final URI inputUri, final Path outpath) throws TranscoderException, IOException {
+		writeSVGAsPNG(SVGDocuments.read(inputUri), outpath);
 	}
 
 	/**
@@ -109,8 +108,8 @@ public final class SVGFileToPNGConverter {
 	 * @param lengthValMatcher
 	 *            The {@link Matcher} object matching the length to normalize
 	 * @param userAgent
-	 *            The {@link UserAgent} instance to use for converting lengths to
-	 *            pixel equivalents.
+	 *            The {@link UserAgent} instance to use for converting lengths
+	 *            to pixel equivalents.
 	 * @return A floating-point value representing the matched length in pixels.
 	 */
 	private static float createPixelLength(final Matcher lengthValMatcher, final UserAgent userAgent) {
@@ -125,14 +124,15 @@ public final class SVGFileToPNGConverter {
 	}
 
 	/**
-	 * Finds the width of the widest <code>svg</code> element and the height of the
-	 * tallest <code>svg</code> element in a given {@link Document} in pixels.
+	 * Finds the width of the widest <code>svg</code> element and the height of
+	 * the tallest <code>svg</code> element in a given {@link Document} in
+	 * pixels.
 	 *
 	 * @param doc
 	 *            The {@code Document} to find the maximum dimensions for.
 	 * @param userAgent
-	 *            The {@link UserAgent} instance to use for converting lengths to
-	 *            pixel equivalents.
+	 *            The {@link UserAgent} instance to use for converting lengths
+	 *            to pixel equivalents.
 	 * @return A two-element array of <code>{width, height}</code>.
 	 */
 	private static float[] findMaxDimensions(final Document doc, final UserAgent userAgent) {
