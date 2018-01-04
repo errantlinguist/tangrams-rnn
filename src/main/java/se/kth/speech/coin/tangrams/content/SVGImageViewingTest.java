cc0d984cc22a0c9c4c9de3fbc64d21f584255949
Todd Shore <tcshore@kth.se>
2018-01-04 10:29:31 +0100

    added usage message; cleanup

--- src/main/java/se/kth/speech/coin/tangrams/content/SVGImageViewingTest.java
+++ src/main/java/se/kth/speech/coin/tangrams/content/SVGImageViewingTest.java
@@ -40,33 +40,40 @@ import org.w3c.dom.svg.SVGDocument;
  */
 public final class SVGImageViewingTest {
 
-//	private static final Logger LOGGER = LoggerFactory.getLogger(SVGImageViewingTest.class);
+	// private static final Logger LOGGER =
+	// LoggerFactory.getLogger(SVGImageViewingTest.class);
 
 	private static final StyleDeclarationParser STYLE_DECLARATION_PARSER = new StyleDeclarationParser();
 
 	public static void main(final String[] args) throws IOException {
-		final Path infilePath = Paths.get(args[0]);
-		final SVGDocument doc = SVGDocuments.read(infilePath.toUri().toString());
-		// Change the properties of the SVG document before rendering
-		setPathStyles(doc, "fill", "purple");
-		final JFrame frame = new JFrame("Image viewer");
-		final JSVGCanvas canvas = new JSVGCanvas();
-//		canvas.setDocumentState(JSVGComponent.ALWAYS_DYNAMIC);
-		canvas.setDocument(doc);
-		final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		final int minDim = Math.min(screenSize.width, screenSize.height);
-		final int imgDim = Math.toIntExact(Math.round(Math.floor(minDim * 0.8f)));
-		// NOTE: The "width" and "height" attributes of the SVG document need not be
-		// changed in order to change the display size
-		canvas.setPreferredSize(new Dimension(imgDim, imgDim));
-		frame.add(canvas);
+		if (args.length != 1) {
+			System.err.println(String.format("Usage: %s <infile>", SVGImageViewingTest.class.getName()));
+			System.exit(64);
+		} else {
+			final Path infilePath = Paths.get(args[0]);
+			final SVGDocument doc = SVGDocuments.read(infilePath.toUri().toString());
+			// Change the properties of the SVG document before rendering
+			setPathStyles(doc, "fill", "purple");
+			final JFrame frame = new JFrame("Image viewer");
+			final JSVGCanvas canvas = new JSVGCanvas();
+			// canvas.setDocumentState(JSVGComponent.ALWAYS_DYNAMIC);
+			canvas.setDocument(doc);
+			final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+			final int minDim = Math.min(screenSize.width, screenSize.height);
+			final int imgDim = Math.toIntExact(Math.round(Math.floor(minDim * 0.8f)));
+			// NOTE: The "width" and "height" attributes of the SVG document
+			// need not be
+			// changed in order to change the display size
+			canvas.setPreferredSize(new Dimension(imgDim, imgDim));
+			frame.add(canvas);
 
-		EventQueue.invokeLater(() -> {
-			frame.pack();
-			frame.setLocationByPlatform(true);
-			frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-			frame.setVisible(true);
-		});
+			EventQueue.invokeLater(() -> {
+				frame.pack();
+				frame.setLocationByPlatform(true);
+				frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+				frame.setVisible(true);
+			});
+		}
 	}
 
 	/**
@@ -93,8 +100,8 @@ public final class SVGImageViewingTest {
 	 * @param value
 	 *            The new value of the property.
 	 * @param priority
-	 *            The new priority of the property (e.g. "important") or the empty
-	 *            string if none.
+	 *            The new priority of the property (e.g. "important") or the
+	 *            empty string if none.
 	 */
 	private static void setPathStyles(final Document doc, final String propertyName, final String value,
 			final String priority) {
@@ -105,22 +112,26 @@ public final class SVGImageViewingTest {
 		}
 	}
 
-//	private static void setSize(final SVGDocument doc, final float width, final float height, final String unit) {
-//		final SVGSVGElement rootElem = doc.getRootElement();
-//		setSize(rootElem, width + unit, height + unit);
-//	}
-//
-//	private static void setSize(final SVGSVGElement elem, final String width, final String height) {
-//		// This has been tested; The property changes are in fact persisted
-//		final String tag = elem.getTagName();
-//		LOGGER.info("Original dimensions of element \"{}\" are {} * {}.", tag,
-//				elem.getWidth().getBaseVal().getValueAsString(), elem.getHeight().getBaseVal().getValueAsString());
-//		// https://xmlgraphics.apache.org/batik/faq.html#changes-are-not-rendered
-//		elem.setAttributeNS(null, "width", width);
-//		elem.setAttributeNS(null, "height", height);
-//		LOGGER.info("New dimensions of element \"{}\" are {} * {}.", tag,
-//				elem.getWidth().getBaseVal().getValueAsString(), elem.getHeight().getBaseVal().getValueAsString());
-//	}
+	// private static void setSize(final SVGDocument doc, final float width,
+	// final float height, final String unit) {
+	// final SVGSVGElement rootElem = doc.getRootElement();
+	// setSize(rootElem, width + unit, height + unit);
+	// }
+	//
+	// private static void setSize(final SVGSVGElement elem, final String width,
+	// final String height) {
+	// // This has been tested; The property changes are in fact persisted
+	// final String tag = elem.getTagName();
+	// LOGGER.info("Original dimensions of element \"{}\" are {} * {}.", tag,
+	// elem.getWidth().getBaseVal().getValueAsString(),
+	// elem.getHeight().getBaseVal().getValueAsString());
+	// // https://xmlgraphics.apache.org/batik/faq.html#changes-are-not-rendered
+	// elem.setAttributeNS(null, "width", width);
+	// elem.setAttributeNS(null, "height", height);
+	// LOGGER.info("New dimensions of element \"{}\" are {} * {}.", tag,
+	// elem.getWidth().getBaseVal().getValueAsString(),
+	// elem.getHeight().getBaseVal().getValueAsString());
+	// }
 
 	/**
 	 *
@@ -131,8 +142,8 @@ public final class SVGImageViewingTest {
 	 * @param value
 	 *            The new value of the property.
 	 * @param priority
-	 *            The new priority of the property (e.g. "important") or the empty
-	 *            string if none.
+	 *            The new priority of the property (e.g. "important") or the
+	 *            empty string if none.
 	 */
 	private static void setStyle(final Node node, final String propertyName, final String value,
 			final String priority) {
