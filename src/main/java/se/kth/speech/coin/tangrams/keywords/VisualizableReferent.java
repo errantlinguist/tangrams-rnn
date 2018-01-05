524ff51a046786e73e53bbdab5e5c7997d352b71
Todd Shore <tcshore@kth.se>
2018-01-05 09:36:24 +0100

    added class header

--- src/main/java/se/kth/speech/coin/tangrams/keywords/VisualizableReferent.java
+++ src/main/java/se/kth/speech/coin/tangrams/keywords/VisualizableReferent.java
@@ -1,3 +1,18 @@
+/*
+ * 	Copyright 2018 Todd Shore
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ */
 package se.kth.speech.coin.tangrams.keywords;
 
 import java.util.concurrent.ConcurrentHashMap;
@@ -5,6 +20,11 @@ import java.util.concurrent.ConcurrentMap;
 
 import se.kth.speech.coin.tangrams.wac.data.Referent;
 
+/**
+ * @author <a href="mailto:tcshore@kth.se">Todd Shore</a>
+ * @since 4 Jan 2018
+ *
+ */
 final class VisualizableReferent {
 
 	private static final ConcurrentMap<Referent, VisualizableReferent> INSTANCES = new ConcurrentHashMap<>();
