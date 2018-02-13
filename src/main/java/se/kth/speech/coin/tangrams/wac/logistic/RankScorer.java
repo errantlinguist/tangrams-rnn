13f2d6244bb0158b4ce643ee53ed2486c1380c25
Todd Shore <tcshore@kth.se>
2018-02-13 11:14:21 +0100

    now re-using existing Instances array instead of re-creating individual vectors for each word * ref classification combination

--- src/main/java/se/kth/speech/coin/tangrams/wac/logistic/RankScorer.java
+++ src/main/java/se/kth/speech/coin/tangrams/wac/logistic/RankScorer.java
@@ -25,6 +25,7 @@ import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.stream.DoubleStream;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
@@ -41,7 +42,7 @@ import se.kth.speech.coin.tangrams.wac.logistic.LogisticModel.Scorer;
 import se.kth.speech.coin.tangrams.wac.logistic.LogisticModel.TrainingData;
 import se.kth.speech.coin.tangrams.wac.logistic.LogisticModel.WordClassifiers;
 import weka.classifiers.functions.Logistic;
-import weka.core.Instance;
+import weka.core.Instances;
 
 public final class RankScorer
 		implements Function<SessionSet, Stream<RoundEvaluationResult<RankScorer.ClassificationResult>>> {
@@ -263,44 +264,56 @@ public final class RankScorer
 		} else {
 			final TrainingData trainingData = updatedTrainingDataFuture.join();
 			final WordClassifiers wordClassifiers = trainingData.getWordClassifiers();
-			final FeatureAttributeData featureAttrs = trainingData.getFeatureAttrs();
 			final Vocabulary vocab = trainingData.getVocabulary();
 			final Object2LongMap<String> wordObservationCounts = createWordObservationCountMap(words, vocab);
 			final String[] oovObservations = words.stream().filter(word -> wordObservationCounts.getLong(word) < 1L)
 					.toArray(String[]::new);
 
+			final List<Referent> refs = round.getReferents();
+			final Instances refInsts = trainingData.getFeatureAttrs().createInstances(refs);
 			// NOTE: Values are retrieved directly from the map instead of
 			// e.g. assigning them to a final field because it's possible
 			// that the map values change at another place in the code and
 			// performance isn't an issue here anyway
 			final boolean weightByFreq = (Boolean) modelParams.get(ModelParameter.WEIGHT_BY_FREQ);
+			// For each word, create an array of scores for each referent
 			// NOTE: Logistic instances are not thread-safe!
-			final Stream<Weighted<Referent>> scoredRefs = round.getReferents().stream().map(ref -> {
-				final Instance inst = featureAttrs.createInstance(ref);
-				final double score = words.stream().mapToDouble(word -> {
-					final double wordScore;
+			final double[][] wordRefScores = words.stream().map(word -> {
+				final double[] refScores;
 
-					final Optional<Logistic> optWordClassifier = wordClassifiers.getWordClassifier(word);
-					if (optWordClassifier.isPresent()) {
-						final Logistic wordClassifier = optWordClassifier.get();
-						assert wordClassifier != null;
-						final double unweightedWordScore = scorer.score(wordClassifier, inst);
-						if (weightByFreq) {
-							final long extantWordObservationCount = wordObservationCounts.getLong(word);
-							wordScore = unweightedWordScore * Math.log10(extantWordObservationCount);
-						} else {
-							wordScore = unweightedWordScore;
-						}
+				final Optional<Logistic> optWordClassifier = wordClassifiers.getWordClassifier(word);
+				if (optWordClassifier.isPresent()) {
+					final Logistic wordClassifier = optWordClassifier.get();
+					assert wordClassifier != null;
+					final DoubleStream unweightedRefScores = scorer.score(wordClassifier, refInsts);
+					if (weightByFreq) {
+						final long extantWordObservationCount = wordObservationCounts.getLong(word);
+						refScores = unweightedRefScores.map(score -> score * Math.log10(extantWordObservationCount))
+								.toArray();
 					} else {
-						wordScore = 0.0;
+						refScores = unweightedRefScores.toArray();
 					}
+				} else {
+					refScores = new double[refInsts.size()];
+				}
 
-					return wordScore;
-				}).average().orElse(0.0);
-				return new Weighted<>(ref, score);
-			});
-			@SuppressWarnings("unchecked")
-			final List<Weighted<Referent>> scoredRefList = Arrays.asList(scoredRefs.toArray(Weighted[]::new));
+				return refScores;
+			}).toArray(double[][]::new);
+
+			final List<Weighted<Referent>> scoredRefList = new ArrayList<>(refs.size());
+			for (final ListIterator<Referent> refIter = refs.listIterator(); refIter.hasNext();) {
+				final int refIdx = refIter.nextIndex();
+				final Referent ref = refIter.next();
+				final Stream<double[]> wordRefScoreArrays = Arrays.stream(wordRefScores);
+				// For each row representing a word, get the column representing
+				// the score for the given referent
+				final DoubleStream wordScores = wordRefScoreArrays
+						.mapToDouble(wordRefScoreArray -> wordRefScoreArray[refIdx]);
+				final double refScore = wordScores.average().orElse(0.0);
+				scoredRefList.add(new Weighted<Referent>(ref, refScore));
+			}
+			assert refs.size() == scoredRefList.size();
+
 			final Object2LongMap<String> interactiondata = trainingData.getInteractionData();
 			result = Optional.of(new ClassificationResult(scoredRefList, words, oovObservations, wordObservationCounts,
 					trainingData.getBackgroundDataTokenCount(), sumValues(interactiondata)));
